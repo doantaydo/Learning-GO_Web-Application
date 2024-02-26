de2c24cb003f474a5a417b342ade159e12823230
AkihitoShu <baikadodo2001@gmail.com>
2024-02-27 00:12:04 +0700

    Convert HTML files to GO templates, add CSRF protection & handler for search-availability form

--- Hotel-Bookings/packages/handlers/handlers.go
+++ Hotel-Bookings/packages/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/doantaydo/Learning-GO_Web-Application/Hotel-Bookings/packages/config"
@@ -28,23 +29,44 @@ func NewHandlers(r *Repository) {
 }
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	stringMap := make(map[string]string)
-	stringMap["test"] = "Hello."
+	// stringMap := make(map[string]string)
+	// stringMap["test"] = "Hello."
 
-	remoteID := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteID)
+	// remoteID := r.RemoteAddr
+	// m.App.Session.Put(r.Context(), "remote_ip", remoteID)
 
-	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{StringMap: stringMap})
+	render.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	stringMap := make(map[string]string)
-	stringMap["test"] = "Hello, again."
+	render.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{})
+}
 
-	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
-	stringMap["remote_ip"] = remoteIP
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "generals.page.tmpl", &models.TemplateData{})
+}
 
-	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{StringMap: stringMap})
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "majors.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "search-availability.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	start := r.Form.Get("start")
+	end := r.Form.Get("end")
+	fmt.Println(r)
+	w.Write([]byte(fmt.Sprintf("start date is %s and end is %s", start, end)))
+}
+
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "contact.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) MakeReservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) EmptyFunc(w http.ResponseWriter, r *http.Request) {
--- readme.md
+++ readme.md
@@ -5,6 +5,7 @@ This is the  repository for my learning course in Udemy.
 - Instructor: [Ph.D. Trevor Sawler](https://www.udemy.com/course/building-modern-web-applications-with-go/#instructor-1)
 
 In this course, I built a Basic Web Application and a Bookings and Reservations Web Application.
+- Uses GO version 1.21.5
 - Uses the [chi router](https://github.com/go-chi/chi)
 - Uses [alex edwards SCS](https://github.com/alexadwards/scs/v2) session management
 - Uses [nosurf](https://github.com/justinas/nosurf)
