cda70ab01371bd494bf7924a819f9a49145c20af
AkihitoShu <baikadodo2001@gmail.com>
2024-10-14 20:54:37 +0700

    + Added handler to change status of reservation to process

--- Hotel-Bookings/internal/handlers/handlers.go
+++ Hotel-Bookings/internal/handlers/handlers.go
@@ -17,6 +17,7 @@ import (
 	"github.com/doantaydo/Learning-GO_Web-Application/Hotel-Bookings/internal/render"
 	"github.com/doantaydo/Learning-GO_Web-Application/Hotel-Bookings/internal/repository"
 	"github.com/doantaydo/Learning-GO_Web-Application/Hotel-Bookings/internal/repository/dbrepo"
+	"github.com/go-chi/chi"
 )
 
 type Repository struct {
@@ -564,6 +565,17 @@ func (m *Repository) AdminPostShowReservation(w http.ResponseWriter, r *http.Req
 // AdminReservationsCalendar shows reservations calendar
 func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "admin-reservations-calendar.page.tmpl", &models.TemplateData{})
+}
+
+// AdminProcessReservation marks a reservation as processed
+func (m *Repository) AdminProcessReservation(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+	src := chi.URLParam(r, "src")
+
+	_ = m.DB.UpdateProcessedForReservation(id, 1)
+	m.App.Session.Put(r.Context(), "flash", "Reservation marked as processed")
+
+	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-%s", src), http.StatusSeeOther)
 }
 
 func (m *Repository) EmptyFunc(w http.ResponseWriter, r *http.Request) {
