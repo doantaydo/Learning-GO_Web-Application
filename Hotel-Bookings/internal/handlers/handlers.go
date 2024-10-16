805ce4441d3d1a1a82efd90d914327dd4af840a3
AkihitoShu <baikadodo2001@gmail.com>
2024-10-17 01:28:20 +0700

    + Added handlers to add/remove block room in room_restrictions

--- Hotel-Bookings/internal/handlers/handlers.go
+++ Hotel-Bookings/internal/handlers/handlers.go
@@ -631,11 +631,27 @@ func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Re
 			if y.ReservationID > 0 {
 				// it's a reservation
 				for d := y.StartDate; d.After(y.EndDate) == false; d = d.AddDate(0, 0, 1) {
+					if d.AddDate(0, 0, 1).After(y.EndDate) == true {
+						break
+					}
 					reservationMap[d.Format("2006-01-02")] = y.ReservationID
 				}
 			} else {
 				// it's a block
-				blockMap[y.StartDate.Format("2006-01-02")] = y.ID
+				//blockMap[y.StartDate.Format("2006-01-02")] = y.ID
+				// if y.StartDate.AddDate(0, 0, 2).After(y.EndDate) == true {
+				// 	blockMap[y.StartDate.Format("2006-01-02")] = y.ID
+				// } else {
+				// 	for d := y.StartDate; d.After(y.EndDate) == false; d = d.AddDate(0, 0, 1) {
+				// 		blockMap[d.Format("2006-01-02")] = y.ID
+				// 	}
+				// }
+				for d := y.StartDate; d.After(y.EndDate) == false; d = d.AddDate(0, 0, 1) {
+					if d.AddDate(0, 0, 1).After(y.EndDate) == true {
+						break
+					}
+					blockMap[d.Format("2006-01-02")] = y.ID
+				}
 			}
 		}
 		data[fmt.Sprintf("reservation_map_%d", x.ID)] = reservationMap
@@ -649,6 +665,70 @@ func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Re
 		Data:      data,
 		IntMap:    intMap,
 	})
+}
+
+// AdminPostReservationsCalendar handles post of reservations calendar
+func (m *Repository) AdminPostReservationsCalendar(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	year, _ := strconv.Atoi(r.Form.Get("year"))
+	month, _ := strconv.Atoi(r.Form.Get("month"))
+
+	// process blocks
+	rooms, err := m.DB.AllRooms()
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	form := forms.New(r.PostForm)
+
+	for _, room := range rooms {
+		// get the block map from the session.
+		// Loop through the entire map, if we have an entry in the map,
+		// that does not exist in our posted data, and if the restriction id > 0,
+		// then it's a block we need to remove
+		curMap := m.App.Session.Get(r.Context(), fmt.Sprintf("block_map_%d", room.ID)).(map[string]int)
+		for name, value := range curMap {
+			// ok will be false if the value is not in the map
+			if val, ok := curMap[name]; ok {
+				// only pay attention to values > 0, and that are not in the form post
+				// the rest are just placeholders fpr days without blocks
+				if val > 0 {
+					if !form.Has(fmt.Sprintf("remove_block_%d_%s", room.ID, name)) {
+						// delete the restriction by ID
+						//log.Println("Would delete block ", value)
+						err := m.DB.DeleteBlockByID(value)
+						if err != nil {
+							log.Println(err)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// handle for new blocks
+	for name, _ := range r.PostForm {
+		if strings.HasPrefix(name, "add_block") {
+			exploded := strings.Split(name, "_")
+			roomID, _ := strconv.Atoi(exploded[2])
+			date, _ := time.Parse("2006-01-2", exploded[3])
+			// insert a new block
+			//log.Println("Would insert block for room id ", roomID, ", for date ", exploded[3])
+			err := m.DB.InsertBlockForRoom(roomID, date)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+
+	m.App.Session.Put(r.Context(), "flash", "Changes saved!")
+	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-calendar?y=%d&m=%d", year, month), http.StatusSeeOther)
 }
 
 // AdminProcessReservation marks a reservation as processed
