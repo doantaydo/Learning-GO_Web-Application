7a4c93afe2d44e761a1afefd8a1e4ee495e1b9be
AkihitoShu <baikadodo2001@gmail.com>
2024-11-20 00:13:36 +0700

    + Added migrations for adding Admin  User To Users Table

---
+++ Hotel-Bookings/migrations/20241119170820_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+	 ('Shu','Akihito','admin@admin.com','$2a$12$JFkjE3cAZxNwye2xssHlM.Yzz89rvwMu.62WkChHgvPrYmbvyISvS',3,'2024-09-23 00:00:00','2023-09-23 00:00:00');
